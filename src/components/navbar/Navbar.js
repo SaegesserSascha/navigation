1c9fbfc5294850df92719c305597a48c93612aa8
SaschaSaegesser <32762408+SaschaSaegesser@users.noreply.github.com>
2021-02-16 17:48:24 +0100

    Topnav is now sticked to top when scrolling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import "./style.css";
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { routes } from "constants/routes";
 
 export default function Navbar() {
+  const location = useLocation(null);
+  const [sticky, setSticky] = useState(false);
+  const ref = useRef(null);
+
+  const handleScroll = () => {
+    if (ref.current) {
+      setSticky(ref.current.getBoundingClientRect().top <= 0);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
   return (
-    <nav>
+    <nav id="nav" className={`app-nav ${sticky ? " sticky" : ""}`} ref={ref}>
       <ul className="app-nav-list">
         {routes.map(({ path, name }, key) =>
-          <li className="app-nav-item" key={key}>
-            <Link to={path}>{name}</Link>
+
+          <li className={`app-nav-item ${location.pathname === path ? " current-nav" : ""}`} key={key}>
+            <Link to={path}>
+              {name}
+            </Link>
           </li>)}
       </ul>
     </nav>
