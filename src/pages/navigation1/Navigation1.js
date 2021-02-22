a7df987de82bf1c7a051af88e2fcff2d01c43f7f
SaschaSaegesser <32762408+SaschaSaegesser@users.noreply.github.com>
2021-02-22 13:14:52 +0100

    Fixed an issue where parts of the navigation would be hidden on smaller devices

--- src/pages/navigation1/Navigation1.js
+++ src/pages/navigation1/Navigation1.js
@@ -26,7 +26,7 @@ export default function Navigation1() {
   }, []);
 
   return (
-    <div className="navigation1-container">
+    <div>
       <nav id="nav1" className={`navigation1 ${sticky ? " sticky" : ""}`} ref={ref}>
         <ul className="navigation1-list">
           {routes.map(({ path, name, children = [] }, key) => {
